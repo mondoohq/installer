9569c105c985ca530dff8060d3264fc1b78130a2
Christoph Hartmann <chris@lollyrock.com>
2020-04-21 20:43:10 +0200

    update to mondoo 0.45.1

--- download.sh
+++ download.sh
@@ -41,7 +41,7 @@ This script source is available at: https://github.com/mondoolabs/mondoo
 
 base_url="${MONDOO_MIRROR:-https://releases.mondoo.io}"
 product="mondoo"
-version="${MONDOO_VERSION:-0.45.0}"
+version="${MONDOO_VERSION:-0.45.1}"
 
 fail() {
   echo -e "${red}${1}${end}";
