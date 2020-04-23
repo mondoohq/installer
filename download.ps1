8b5f3e501388f6651ce49449a4e6d14de76f0df5
Christoph Hartmann <chris@lollyrock.com>
2020-04-23 12:17:00 +0200

    update to mondoo 0.46.0

--- download.ps1
+++ download.ps1
@@ -89,7 +89,7 @@ function setenv($name,$value,$global) {
 $dir = Get-Location
 
 # manual override
-# $version = '0.45.1'
+# $version = '0.46.0'
 # $arch = 'amd64'
 # $releaseurl = "https://releases.mondoo.io/mondoo/${version}/mondoo_${version}_windows_${arch}.zip"
 
