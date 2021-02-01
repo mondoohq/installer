926cbddac110311538bebff61f3e31b263bf9f1d
Christoph Hartmann <chris@lollyrock.com>
2021-02-02 00:58:56 +0100

    bump to mondoo 3.2.0

--- download.ps1
+++ download.ps1
@@ -89,7 +89,7 @@ function setenv($name,$value,$global) {
 $dir = Get-Location
 
 # manual override
-# $version = '3.0.2'
+# $version = '3.2.0'
 # $arch = 'amd64'
 # $releaseurl = "https://releases.mondoo.io/mondoo/${version}/mondoo_${version}_windows_${arch}.zip"
 
--- install.ps1
+++ install.ps1
@@ -12,7 +12,7 @@
     If provided, tries to download the specific version instead of the latest
     .EXAMPLE
     Import-Module ./install.ps1; Install-Mondoo -RegistrationToken INSERTKEYHERE
-    Import-Module ./install.ps1; Install-Mondoo -Version 3.0.2
+    Import-Module ./install.ps1; Install-Mondoo -Version 3.2.0
 #>
 function Install-Mondoo {
   [CmdletBinding()]
