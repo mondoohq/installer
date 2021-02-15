e273011db5e788024cfea84fb4795da36de65f48
Christoph Hartmann <chris@lollyrock.com>
2021-02-15 11:11:42 +0100

    bump to mondoo 3.3.1

--- download.ps1
+++ download.ps1
@@ -89,7 +89,7 @@ function setenv($name,$value,$global) {
 $dir = Get-Location
 
 # manual override
-# $version = '3.2.0'
+# $version = '3.3.1'
 # $arch = 'amd64'
 # $releaseurl = "https://releases.mondoo.io/mondoo/${version}/mondoo_${version}_windows_${arch}.zip"
 
