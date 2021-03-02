1742c867e1f85b57f9f268e82242a808156dbc96
Christoph Hartmann <chris@lollyrock.com>
2021-03-02 22:35:16 +0100

    add terraform noop example

---
+++ examples/terraform-noop/vars.tf
@@ -0,0 +1,20 @@
+
+variable "conn" {
+  type = string
+  default = "ssh"
+}
+
+variable "user" {
+  type = string
+  default = "ec2-user"
+}
+
+variable "password" {
+  type = string
+  default = ""
+}
+
+variable "host" {
+  type = string
+  default = "52.31.244.47"
+}
