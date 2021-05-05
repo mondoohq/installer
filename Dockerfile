075e7c72312b4e051d1b4a70b54c487cf3169403
Christoph Hartmann <chris@lollyrock.com>
2021-05-05 15:50:47 +0200

    bump to mondoo 4.5.0

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine:3.13
-ARG VERSION=4.4.1
+ARG VERSION=4.5.0
 ARG PACKAGE="mondoo_${VERSION}_linux_amd64.tar.gz"
 ARG BASEURL="https://releases.mondoo.io/mondoo/${VERSION}"
 RUN apk update &&\
--- install.ps1
+++ install.ps1
@@ -12,7 +12,7 @@
     If provided, tries to download the specific version instead of the latest
     .EXAMPLE
     Import-Module ./install.ps1; Install-Mondoo -RegistrationToken INSERTKEYHERE
-    Import-Module ./install.ps1; Install-Mondoo -Version 4.4.1
+    Import-Module ./install.ps1; Install-Mondoo -Version 4.5.0
 #>
 function Install-Mondoo {
   [CmdletBinding()]
