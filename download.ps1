924b2e66efed7e45669a1cbd9c61f558ab1bc062
Christoph Hartmann <chris@lollyrock.com>
2021-07-08 16:54:00 +0200

    mondoo 4.10.0

--- download.ps1
+++ download.ps1
@@ -89,7 +89,7 @@ function setenv($name,$value,$global) {
 $dir = Get-Location
 
 # manual override
-# $version = '4.9.0'
+# $version = '4.10.0'
 # $arch = 'amd64'
 # $releaseurl = "https://releases.mondoo.io/mondoo/${version}/mondoo_${version}_windows_${arch}.zip"
 
