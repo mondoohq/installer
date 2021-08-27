fed17fbc73eb1b2c2469b31b8405fc13e7bba107
Victoria Jeffrey <vj@email.com>
2021-08-27 12:22:47 -0600

    4.17.1

--- download.ps1
+++ download.ps1
@@ -88,7 +88,7 @@ function setenv($name,$value,$global) {
 $dir = Get-Location
 
 # manual override
-# $version = '4.17.0'
+# $version = '4.17.1'
 # $arch = 'amd64'
 # $releaseurl = "https://releases.mondoo.io/mondoo/${version}/mondoo_${version}_windows_${arch}.zip"
 
