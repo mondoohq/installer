a265ebfe4b980b4a3d452c9cc060f700c07e32e8
Victoria Jeffrey <vj@email.com>
2021-10-04 17:05:16 -0600

    5.7.0
    Signed-off-by: Victoria Jeffrey <vj@email.com>

--- install.ps1
+++ install.ps1
@@ -13,7 +13,7 @@
     If provided, tries to download the specific version instead of the latest
     .EXAMPLE
     Import-Module ./install.ps1; Install-Mondoo -RegistrationToken INSERTKEYHERE
-    Import-Module ./install.ps1; Install-Mondoo -Version 5.6.0
+    Import-Module ./install.ps1; Install-Mondoo -Version 5.7.0
 #>
 function Install-Mondoo {
   [CmdletBinding()]
