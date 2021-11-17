8a4cd974dc2cde28557707370f058f3f12d6532c
Jay Mundrawala <jay@thechamberofunderstanding.com>
2021-11-17 13:06:36 -0600

    5.14.0

--- download.sh
+++ download.sh
@@ -56,7 +56,7 @@ This script source is available at: https://github.com/mondoolabs/mondoo
 
 base_url="${MONDOO_MIRROR:-https://releases.mondoo.io}"
 product="mondoo"
-version="${MONDOO_VERSION:-5.13.0}"
+version="${MONDOO_VERSION:-5.14.0}"
 
 fail() {
   echo -e "${red}${1}${end}";
