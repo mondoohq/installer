fd2b6f29f2983d7acee374b4f6cdb486054090f4
Christoph Hartmann <chris@lollyrock.com>
2021-11-30 13:09:11 +0100

    🧹 move chef-mondoo into its own repository
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- deployment/chef-mondoo/test/cookbooks/mondoo-test/metadata.rb
+++
@@ -1,3 +0,0 @@
-name 'mondoo-test'
-version '0.1.0'
-chef_version '>= 14'
--- deployment/chef-mondoo/test/cookbooks/mondoo-test/recipes/default.rb
+++
@@ -1,3 +0,0 @@
-# see https://docs.chef.io/environment_variables.html
-ENV['LANG'] = 'en_US.utf-8'
-ENV['LC_ALL'] = 'en_US.utf-8'
