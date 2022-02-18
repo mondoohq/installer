796aa1b5de9e5e29b144ecba5e4639a5de9714a3
Ben Rockwood <benr@cuddletech.com>
2022-02-17 18:45:15 -0800

    Update copyright year to 2022
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2021 Mondoo, Inc.
+# Copyright (c) 2019-2022 Mondoo, Inc.
 # License: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
