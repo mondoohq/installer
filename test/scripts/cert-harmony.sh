0aa7048789a59c75da22b7dc774d68a6bd8859ce
Ben Rockwood <benr@cuddletech.com>
2023-01-31 04:57:21 -0800

    🧹 Do not ASCII armor keys for diff (#254)
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>
    Co-authored-by: Christoph Hartmann <chris@lollyrock.com>

--- test/scripts/cert-harmony.sh
+++ test/scripts/cert-harmony.sh
@@ -8,10 +8,12 @@ mkdir ${DIR}
 cd ${DIR} || exit 1
 
 curl -s -o mondoohq-installer-cert.asc https://raw.githubusercontent.com/mondoohq/installer/main/public-package-signing.gpg
+echo "REFERENCE KEY:"
+< mondoohq-installer-cert.asc gpg --show-keys --no-default-keyring
+echo "--------------"
 
 printf "=> Comparing Github Installer vs RPM Repo..."
-curl -s -o mondoo-repo-rpm.gpg https://releases.mondoo.com/rpm/pubkey.gpg
-gpg  --keyring ./mondoo-repo-rpm.gpg --no-default-keyring --export -a > mondoo-repo-rpm.asc
+curl -s -o mondoo-repo-rpm.asc https://releases.mondoo.com/rpm/pubkey.gpg
 if diff mondoohq-installer-cert.asc mondoo-repo-rpm.asc >/dev/null; then
 	echo "PASS"
 else
@@ -20,8 +22,7 @@ else
 fi
 
 printf "=> Comparing Github Installer vs Deb Repo..."
-curl -s -o mondoo-repo-deb.gpg https://releases.mondoo.com/debian/pubkey.gpg
-gpg  --keyring ./mondoo-repo-deb.gpg --no-default-keyring --export -a > mondoo-repo-deb.asc
+curl -s -o mondoo-repo-deb.asc https://releases.mondoo.com/debian/pubkey.gpg
 if diff mondoohq-installer-cert.asc mondoo-repo-deb.asc >/dev/null; then
 	echo "PASS"
 else
