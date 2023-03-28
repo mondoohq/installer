3101037722153e934f138ad409aa29f9c4b2af3f
Charles Johnson <github@chipse.cx>
2023-03-28 11:53:10 -0700

    Adds a friendlier deprecation message (#293)
    Adds a friendlier deprecation message
    
    Signed-off-by: Charles Johnson <chip@mondoo.com>

--- helper/mondoo.ps1
+++ helper/mondoo.ps1
@@ -1,6 +1,7 @@
 #!/usr/bin/env pwsh
 $fc = $host.UI.RawUI.ForegroundColor
 $host.UI.RawUI.ForegroundColor = "red"
-Write-Output "Mondoo command is deprecated. Use cnspec instead, Mondoo's cloud-native security scanner and CLI."
+Write-Output "The mondoo command is now cnspec, the cloud-native specification & security scanner. Please call the cnspec binary directly. Support for invoking cnspec by calling mondoo will be removed in a future version."
 $host.UI.RawUI.ForegroundColor = $fc
 cnspec $args
+
--- helper/mondoo.sh
+++ helper/mondoo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
-printf "\033[0;31mMondoo command is deprecated. Use cnspec instead, Mondoo's cloud-native security scanner and CLI.\033[0m\n"
+printf "\033[0;31mThe mondoo command is now cnspec, the cloud-native specification & security scanner. Please call the cnspec binary directly. Support for invoking cnspec by calling mondoo will be removed in a future version.\033[0m\n"
 cnspec "$@"
