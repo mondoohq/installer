93bd832ea8f034f188c999fbd536d262f3580723
Ben Rockwood <benr@cuddletech.com>
2023-07-25 02:50:49 -0700

    Chocolatey port
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

---
+++ packages/chocolatey/cnspec-generate.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+if [[ $VERSION = "" ]]; then
+	echo "ERROR: You must supply a version number"
+	exit 1
+fi
+
+
+
+rm -rf cnspec/
+mkdir cnspec && cd cnspec
+
+echo "Generating NuSpec"
+cat >cnspec.nuspec <<NUSPEC
+<?xml version="1.0" encoding="utf-8"?>
+
+<package xmlns="http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd">
+  <metadata>
+    <id>cnspec</id>
+    <version>${VERSION}</version>
+    <packageSourceUrl>https://github.com/mondoohq/cnspec</packageSourceUrl>
+    <owners>Mondoo</owners>
+    <dependencies>
+      <dependency id="cnquery" />
+    </dependencies>
+
+    <title>Mondoo cnspec</title>
+    <authors>Mondoo</authors>
+    <projectUrl>https://github.com/mondoohq/cnspec</projectUrl>
+    <iconUrl>https://mondoo.com/mondoo_choco_logo.jpg</iconUrl>
+    <copyright>2023 Mondoo, Inc.</copyright>
+    <licenseUrl>https://github.com/mondoohq/cnspec/blob/main/LICENSE.txt</licenseUrl>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <docsUrl>https://mondoo.com/docs/cnspec/</docsUrl>
+    <bugTrackerUrl>https://github.com/mondoohq/cnspec/issues</bugTrackerUrl>
+    <releaseNotes>[Release Notes](https://mondoo.com/releases/)</releaseNotes>
+    <tags>cnspec mondoo security compliance cloud kubernetes server</tags>
+    <summary>cnspec is an open source, cloud-native tool that evaluates the security of your entire infrastructure. Using intuitive policy as code, cnspec scans everything and identifies gaps that attackers can use to breach your systems.</summary>
+    <description>cnspec is an open source, cloud-native tool that evaluates the security of your entire infrastructure. Using intuitive policy as code, cnspec scans everything and identifies gaps that attackers can use to breach your systems. Scan public and private cloud environments, Kubernetes clusters, containers, container registries, servers and endpoints, SaaS products, infrastructure as code, APIs, and more.</description>
+  </metadata>
+  <files>
+    <file src="tools\**" target="tools" />
+  </files>
+</package>
+NUSPEC
+
+
+CHECKSUM=`curl -s https://install.mondoo.com/package/cnspec/windows/amd64/zip/${VERSION}/sha256`
+
+echo "Generating Install Script"
+mkdir tools
+cat >tools/chocolateyInstall.ps1 <<CHOCOSTALL
+\$ErrorActionPreference = 'Stop'; # stop on all errors
+\$toolsDir   = "\$(Split-Path -parent \$MyInvocation.MyCommand.Definition)"
+
+\$version  = '${VERSION}'
+\$url      = "https://releases.mondoo.com/cnspec/${VERSION}/cnspec_${VERSION}_windows_amd64.zip"
+\$checksum = '${CHECKSUM}'
+
+\$packageArgs = @{
+  packageName   = \$env:ChocolateyPackageName
+  unzipLocation = \$toolsDir
+  url64bit      = \$url
+
+  checksum64    = \$checksum
+  checksumType64= 'sha256' #default is checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+CHOCOSTALL
