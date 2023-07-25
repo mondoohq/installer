4aa5ff45baa2ee3d9e12859603cab8943081265a
Ben Rockwood <benr@cuddletech.com>
2023-07-25 02:08:31 -0700

    Remove Mondoo Shim
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

--- helper/mondoo.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-$fc = $host.UI.RawUI.ForegroundColor
-$host.UI.RawUI.ForegroundColor = "red"
-Write-Output "The mondoo command is now cnspec, the cloud-native specification & security scanner. Please call the cnspec binary directly. Support for invoking cnspec by calling mondoo will be removed in a future version."
-$host.UI.RawUI.ForegroundColor = $fc
-cnspec $args
-
--- helper/mondoo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-printf "\033[0;31mThe mondoo command is now cnspec, the cloud-native specification & security scanner. Please call the cnspec binary directly. Support for invoking cnspec by calling mondoo will be removed in a future version.\033[0m\n"
-cnspec "$@"
