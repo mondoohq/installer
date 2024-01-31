5df0649d5105dec2fe0505a9f2b5da544961b7bd
Ben Rockwood <benr@cuddletech.com>
2024-01-30 17:19:27 -0800

    Build metapackage
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

--- packages/archlinux/generator/main.go
+++ packages/archlinux/generator/main.go
@@ -76,7 +76,7 @@ var products = map[string]Product{
 		Homepage:    "https://mondoo.com",
 		PkgName:     "cnquery",
 		Class:       "Cnquery",
-		License:     "MPL 2.0",
+		License:     "BUSL-1.1",
 	},
 	"cnspec": {
 		LatestUrl:   "https://releases.mondoo.com/cnspec/latest.json?ignoreCache=1",
@@ -84,7 +84,7 @@ var products = map[string]Product{
 		Homepage:    "https://mondoo.com",
 		PkgName:     "cnspec",
 		Class:       "Cnspec",
-		License:     "MPL 2.0",
+		License:     "BUSL-1.1",
 		Depends: []string{
 			"cnquery",
 		},
